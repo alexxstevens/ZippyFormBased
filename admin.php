231083f8c8c98d46e1749f4e3c51f733c7fe284a
alexxstevens <41077253+alexxstevens@users.noreply.github.com>
2020-04-07 20:36:50 -0500

    1-fix delete

--- admin.php
+++ admin.php
@@ -49,9 +49,9 @@
         include('class_list.php');
         include('view/footer.php');
     } else if ($action == 'delete_vehicle') {
-        $product_id        = filter_input(INPUT_POST, 'product_id       ', FILTER_VALIDATE_INT);
-        if ($product_id        == NULL || $product_id        == FALSE) {
-            $error = "Missing or incorrect vehicle id.";
+        $product_id = filter_input(INPUT_POST, 'product_id', FILTER_VALIDATE_INT);
+        if ($product_id == NULL || $product_id == FALSE) {
+            $error = "Missing or incorrect product id.";
             include('errors/error.php');
         } else {
             delete_vehicle($product_id       );
--- view/zippy-links.php
+++ view/zippy-links.php
@@ -5,15 +5,5 @@
     <p><a href="admin.php?action=list_classes">View/Edit Vehicle Classes</a></p>
 </div>
 
-<style> 
 
-div#links {
-  margin-top: 15px;
-  padding-top: 10px;
-  padding-right: 10px;
-  padding-left: 10px;
-  background-color: rgba(192, 192, 192, 0.192);
-  border: 2px solid black;
-  box-shadow: 5px 10px 8px #888888;
-}
-</style>
+
